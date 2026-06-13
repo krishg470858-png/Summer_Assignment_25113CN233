61b549a3ae0443eff5be15c88f7c3a489bb08199
KRISH GUPTA <krishg470858@gmail.com>
2026-06-13 21:27:32 +0530

    Add day 10 folder

---
+++ day 10/40.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter no. of lines for pyramid : ";
+    cin>>n;
+ 
+    for(int i=1; i<=n; i++){
+           
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=0; j<i; j++){
+            cout<<char('A'+j);
+        }
+    
+        for(int j=i-2; j>=0; j--){
+            cout<<char('A'+j);
+            
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
